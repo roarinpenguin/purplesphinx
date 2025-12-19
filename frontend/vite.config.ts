84ccc37fc93813e4e6dc4421e485334cd3cb7380
roarinpenguin <roarinpenguin@roarinpenguin.com>
2025-12-19 01:26:49 +0100

    Allow all hosts in Vite + fix backend port in proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,19 +4,21 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
     strictPort: true,
+    allowedHosts: 'all',
     proxy: {
       '/api': {
-        target: 'http://backend:8080',
+        target: 'http://backend:8882',
         changeOrigin: true
       },
       '/uploads': {
-        target: 'http://backend:8080',
+        target: 'http://backend:8882',
         changeOrigin: true
       },
       '/socket.io': {
-        target: 'http://backend:8080',
+        target: 'http://backend:8882',
         changeOrigin: true,
         ws: true
       }
